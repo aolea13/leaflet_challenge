48ad183733e98befb37a8c0098606d00822103a9
aolea13 <austinolea@gmail.com>
2021-05-10 11:54:50 -0600

    :sparkles: Objects to Hold layers

--- Leflet-Step-1/static/JS/geomaplog.js
+++ Leflet-Step-1/static/JS/geomaplog.js
@@ -12,17 +12,29 @@ var satellite_map = L.titleLayer ('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/
   accessToken: API_KEY
 });
 
-var grayscaleMap = L.titleLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var grayscale_map = L.titleLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
   maxZoom: 15,
   id: 'mapbox.light',
   accessToken: API_KEY
 });
 
-var outdoorsMap = L.titleLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var outdoors_map = L.titleLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
   maxZoom: 15,
   id: 'mapbox.outdoors',
   accessToken: API_KEY
 });
+
+// BaseMaps to Hold Base Layers
+var baseMaps = {
+  'Satellite': satellite_map,
+  'Grayscale': grayscale_map,
+  'Outdoors': outdoors_map
+};
+
+//Object to Hold oVerlay Layers
+var overlayMaps = {
+  'Earthquakes': earthquakes
+}
 
