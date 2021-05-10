b496d1995f6f129565cde53c37b846d7d341445b
aolea13 <austinolea@gmail.com>
2021-05-10 12:24:51 -0600

    :sparkles: Upto Markers

--- Leflet-Step-1/static/JS/geomaplog.js
+++ Leflet-Step-1/static/JS/geomaplog.js
@@ -1,10 +1,10 @@
 // EarthquakesGeo JSON URL Variables
 var earthquakes_URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 
-// Creat layer group
+// Layer group
 var earthquakes = new L.LayerGroup();
 
-// Define Variables for Tile Layers
+// Variables for Tile Layers
 var satellite_map = L.titleLayer ('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
   maxZoom: 15,
@@ -33,8 +33,55 @@ var baseMaps = {
   'Outdoors': outdoors_map
 };
 
-//Object to Hold oVerlay Layers
+// Object to Hold oVerlay Layers
 var overlayMaps = {
   'Earthquakes': earthquakes
 }
 
+// Default Map
+var myMap = L.map('map'{
+  center: [34.0522, -118.2437],
+  zoom: 1,
+  layers: [satellite_map, earthquakes]
+});
+
+// Layer Control
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+// Retrieve Earthquake URL
+d3.json(earthquakes_URL, function(earthquakeData) {
+  
+  //Markers
+  function markerSize(magnitude){
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: chooseColor(feature.properties.mag),
+      color: '#000000',
+      radius: markerSize(feature.properties.mag),
+      stroke: true,
+      weight: 1
+    };
+  }
+  function chooseColor(magnitude) {
+    switch (true){
+      case magnitude > 5:
+          return '#581845';
+      case magnitude > 4:
+          return '#900C3F';
+      case magnitude > 3:
+          return '#C70039';
+      case magnitude > 2:
+          return '#FF5733';
+      case magnitude > 1:
+          return '#FFC300';
+      default:
+          return '#DAF7A6';
+      }
+    }
